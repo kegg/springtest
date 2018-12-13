40bc16d84d2a7a0013fc0cfb374149d819e4cca8
Kyle Eggleston <kyle@dreamsense.com>
2018-12-13 15:09:39 -0700

    Add files via upload

--- src/main/java/com/dreamsense/hello/pages/WelcomeController.java
+++ src/main/java/com/dreamsense/hello/pages/WelcomeController.java
@@ -6,16 +6,22 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.net.URI;
+
 @Controller
 public class WelcomeController {
 
 	// inject via application.properties
 	@Value("${welcome.message:Hello World}")
 	private String message;
+	
+	@Value("${service.url}")
+	private String serviceUrl;
 
 	@RequestMapping("/welcome")
 	public String welcome(Map<String, Object> model) {
 		model.put("message", this.message);
+		model.put("url", URI.create(this.serviceUrl));
 		return "welcome";
 	}
 
